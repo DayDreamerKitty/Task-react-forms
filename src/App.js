8324d1748dab67b709da5384864c5faefa77dfb1
Kitty <zainab_97@hotmail.co.uk>
2021-10-11 19:52:59 +0300

    delete not working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { useState } from 'react'
-import './App.css';
-import Form from './Components/Form'
-import HeaderAndTitle from './Components/HeaderAndTitle'
-import StudentsList from './Components/StudentsList';
+import { useState } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import HeaderAndTitle from "./Components/HeaderAndTitle";
+import StudentsList from "./Components/StudentsList";
 
 function App() {
+  const [studentsInfo, setStudentsInfo] = useState([]);
+  const [show, setShow] = useState(false);
 
-  const [studentsInfo, setStudentsInfo] = useState([])
-  const [show, setShow] = useState(false)
+  const createInfo = (newInfo) => {
+    const id = studentsInfo[studentsInfo.length - 1] + 1;
+    const nInfo = { ...newInfo, id: id };
+    console.log(nInfo);
+    setStudentsInfo([...studentsInfo, nInfo]);
+  };
+
+  const deleteInfo = (id) => {
+    let tempInfo = studentsInfo.filter((student) => student.id !== id);
+    setStudentsInfo(tempInfo);
+  };
 
   return (
-
     <div className="App">
       <HeaderAndTitle showList={setShow} show={show} />
-      {show ? <StudentsList list={studentsInfo} /> : <Form />}
+      {show ? (
+        <StudentsList list={studentsInfo} deleteInfo={deleteInfo} />
+      ) : (
+        <Form createInfo={createInfo} />
+      )}
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,19 +1,70 @@
-import React from 'react'
-import SideInfo from './SideInfo'
+import React from "react";
+import SideInfo from "./SideInfo";
+import { useState } from "react";
 
+function Form({ createInfo }) {
+  const [info, setInfo] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    superPower: "",
+    emailAddress: "",
+  });
 
-function Form() {
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
 
-    return (
-        <div className="form-page">
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createInfo(info);
+    setInfo({
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      superPower: "",
+      emailAddress: "",
+    });
+  };
 
-            <form>
-                {/* to do :  create your form here  */}
-            </form>
-            <SideInfo />
-
-        </div>
-    )
+  return (
+    <div className="form-page">
+      <form onSubmit={handleSubmit}>
+        <input
+          name="firstName"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={info.firstName}
+        />
+        <input
+          name="lastName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={info.lastName}
+        />
+        <input
+          name="phoneNumber"
+          placeholder="Phone Number"
+          onChange={handleChange}
+          value={info.phoneNumber}
+        />
+        <input
+          name="superPower"
+          placeholder="Superpower"
+          onChange={handleChange}
+          value={info.superPower}
+        />
+        <input
+          name="emailAddress"
+          placeholder="E-mail Address"
+          onChange={handleChange}
+          value={info.emailAddress}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <SideInfo />
+    </div>
+  );
 }
 
-export default Form
+export default Form;
